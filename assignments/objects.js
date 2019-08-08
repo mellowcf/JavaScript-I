a66b127c534b0571514ec2bb79a8967a156474bb
mellowcf <mellowcf@gmail.com>
2019-08-07 21:57:04 -0400

    Update objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -72,7 +72,7 @@ console.log(Kennan.speak());
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 Antonietta.multiplyNums = (num1, num2) => num1 * num2;
-console.log(Antonietta.multiplyNums(4, 5));
+console.log(Antonietta.multiplyNums(3, 4));
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ====
